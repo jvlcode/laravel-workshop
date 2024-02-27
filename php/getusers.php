5b1820e9f4522c45ee866717421661dd1f65f7af
JVLcode <81806680+jvlcode@users.noreply.github.com>
2024-02-27 19:20:20 +0530

    getusers DB update

--- php/getusers.php
+++ php/getusers.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database";
+$username = "root";
+$password = "";
+$dbname = "mysite";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -18,9 +18,12 @@ $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo "<table border=1>";
+    echo "<th>ID</th><th>Name</th><th>Email</th>";
     while($row = $result->fetch_assoc()) {
-        echo "User ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"]. "</td><td>" . $row["email"]. "</td></tr>";
     }
+    echo "</table>";
 } else {
     echo "0 results";
 }
